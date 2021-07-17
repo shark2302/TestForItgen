5bedfa4c174a9f9fcd6c639cea8adc2af171388b
e.kravchenko <e.kravchenko@corp.mail.ru>
2021-07-17 20:18:26 +0300

    Task4 solution

---
+++ TestForItgen/Task4/Task4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task4
+{
+	internal class Task4
+	{
+
+		private static int ReadInt()
+		{
+			int result;
+			Console.WriteLine("Введите число: ");
+			while (!Int32.TryParse(Console.ReadLine(), out result))
+			{
+				Console.WriteLine("Это не целое число! Попробуйте еще раз: ");
+			}
+
+			return result;
+		}
+		
+		public static void Main(string[] args)
+		{
+			var r = ReadInt();
+		}
+	}
+}
