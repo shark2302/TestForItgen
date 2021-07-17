863e8431a9087c7afae9454849d47e4e69fd9509
e.kravchenko <e.kravchenko@corp.mail.ru>
2021-07-17 18:17:13 +0300

    Task2 solution

---
+++ TestForItgen/Task2/Task2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml;
+
+namespace Task2
+{
+	internal class Task2
+	{
+		private const string SECRET_MESSAGE = "Secret";
+		private const string PASSWORD = "qw123";
+		private const int ATTEMPT_LIMIT = 3;
+		
+		public static void Main(string[] args)
+		{
+			string password = string.Empty;
+			for (int attempt = 1; attempt <= ATTEMPT_LIMIT ; attempt++)
+			{
+				Console.WriteLine("Введите пароль: ");
+				password = Console.ReadLine();
+				if (password == PASSWORD)
+				{
+					Console.WriteLine("Пароль верный. Секретное слово : " + SECRET_MESSAGE);
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Пароль неверный. Попыток осталось: " + (ATTEMPT_LIMIT - attempt ));
+				}
+			}
+		}
+	}
+}
